b27319fd8863cc07da742333fb44c47d629de2ce
friedooo <friedlich3@gmail.com>
2021-12-01 06:19:44 +0300

    feat: implement get

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,9 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const db = require('../../data/db');
 
-const getAll = async () =>
-  // TODO: mock implementation. should be replaced during task development
-  [];
+const getAll = async () => db.users;
 const create = async (user) => {
   const newUser = { id: uuidv4(), ...user };
   db.users.push(newUser);
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -5,13 +5,19 @@ const usersService = require('./user.service');
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
   // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  res
+    .status(200)
+    .setHeader('content-type', 'application/json')
+    .json(users.map(User.toResponse));
 });
 
 router.route('/').post(async (req, res) => {
   const newUser = await usersService.createUser(req.body);
   // map user fields to exclude secret fields like "password"
-  res.json(User.toResponse(newUser));
+  res
+    .status(201)
+    .setHeader('content-type', 'application/json')
+    .json(User.toResponse(newUser));
 });
 
 module.exports = router;
