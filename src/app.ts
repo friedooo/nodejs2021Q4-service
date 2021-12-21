b44bb742dfc691be49709873c94700dbd24ddd1c
friedooo <friedlich3@gmail.com>
2021-12-22 00:37:46 +0300

    feat: start task

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,19 @@ import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
 
+import CONFIG from './common/config';
+import logger from './utils/logger';
+
 // const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 // app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const app: Application = express();
+
+app.listen(CONFIG.PORT, () => {
+  console.log(`Server is running on http://localhost:${CONFIG.PORT}`);
+  logger.info(`Server started and running on http://localhost:${CONFIG.PORT}`);
+});
+
 app.use(express.json());
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   if (req.originalUrl === '/') {
